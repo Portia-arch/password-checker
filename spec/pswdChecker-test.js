dbf558100cbbe98fcccd2f5faa529ea2599cac38
Portia-arch <portia.mkhabela@umuzi.org>
2019-11-08 12:19:52 +0200

    recent changes

--- spec/pswdChecker-test.js
+++ spec/pswdChecker-test.js
@@ -1,48 +1,58 @@
-const Check = require('../src/pswdChecker');
+const Check = require("../src/pswdChecker");
 
-describe("passwordIsValid", function () {
-    describe("validates the password", function () {
-        it("throws an error when password is empty", function () {
-            expect(Check.passwordIsValid('')).toBe("password should exist");
-        });
-        it("throws an error when password length is less than 9", function () {
-            expect(Check.passwordIsValid("ashgnb")).toBe(
-                "password should be longer than than 8 characters"
-            );
-        });
+describe("passwordIsValid", function() {
+  describe("validates the password", function() {
+    let password = "N4472157o#";
 
-        it("throws an error when the password has no lowercase character", function () {
-            expect(Check.passwordIsValid("123456789")).toBe(
-                "password should have at least one lowercase letter"
-            );
-        });
-
-        it("throws an error when the password has no UPPERCASE character", function () {
-            expect(Check.passwordIsValid("12345678a")).toBe(
-                "password should have at least one uppercase letter"
-            );
-        });
-
-        it("throws an error when the password has no digit", function () {
-            expect(Check.passwordIsValid("babababab~A")).toBe(
-                "password should have at least one digit"
-            );
-        });
-
-        it("throws an error when the password has no symbol", function () {
-            expect(Check.passwordIsValid("12345678aA")).toBe(
-                "password should have at least one symbol"
-            );
-        });
+    it("throws an error when password is empty", function() {
+      expect(Check.passwordIsValid("")).toBe("");
     });
+    it("throws an error when password length is less than 8", function() {
+      expect(
+        Check.passwordIsValid("N4472157o#")
+          .split("")
+          .join("").length
+      ).toBe(">8");
+    });
+
+    it("throws an error when the password has no lowercase character", function() {
+      expect(Check.passwordIsValid("N4472157o#")).toMatch(/[a-z]/);
+    });
+
+    it("throws an error when the password has no UPPERCASE character", function() {
+      expect(Check.passwordIsValid("N4472157o#")).toMatch(/[A-Z]/);
+    });
+
+    it("throws an error when the password has no digit", function() {
+      expect(Check.passwordIsValid("N4472157o#")).toMatch(/[0-9]/);
+    });
+
+    it("throws an error when the password has no symbol", function() {
+      expect(Check.passwordIsValid("N4472157o#")).toMatch(/[@#$%&]/);
+    });
+  });
 });
 
-describe("passwordIsOk", function () {
-    it("returns false if password does not match (cond1 and cond2) and At least one ptn", function () {
-        expect(Check.passwordIsOk("1234")).toBe(false);
-    });
+describe("passwordIsOk", function() {
+  let password = "N4472157o#";
 
-    it("returns true if password match (cond1 and cond2) and At least one ptn", function () {
-        expect(Check.passwordIsOk("@@@@@@@@@@@@@")).toBe(true);
-    });
+  it("throws an error when password is empty", function() {
+    expect(Check.passwordIsValid("")).toMatch("");
+  });
+
+  it("throws an error when password length is less than 8", function() {
+    expect(
+      Check.passwordIsValid("N4472157o#")
+        .split("")
+        .join("").length
+    ).toBe(">8");
+  });
 });
+//     it("returns false if password does not exist", function () {
+//         expect(Check.passwordIsOk('')).toBe(false);
+//     });
+
+//     it("returns true if password match conditions", function () {
+//         expect(Check.passwordIsOk('N4472157o#')).toBe(true);
+//     });
+// });
--- src/pswdChecker.js
+++ src/pswdChecker.js
@@ -1,101 +1,87 @@
 // let condition1 = ("");
 // let condition2 = ("=>9");
-let condition3 = /[a-z]/;
-let condition4 = /[A-Z]/;
-let condition5 = /[@#$%&]/;
-let condition6 = /[0-9]/;
-let error
+let condition1 = /[a-z]/;
+let condition2 = /[A-Z]/;
+let condition3 = /[@#$%&]/;
+let condition4 = /[0-9]/;
+let error;
 
 function passwordIsValid(password) {
-     password = "";
-    try {
-        if (password == '') {
-            throw "password should exist"
-        }
-        else {
-            return password;
-        }
+  //  password = "N4472157o#";
+  try {
+    if (password == "") {
+      throw "password should exist";
+    } else {
+      return password;
     }
-        catch (error) {
-        console.log("error" + error)
-        }
-        try {
-            if (password.length < 9) {
-                throw "password should be longer than than 8 characters"
-            }
-            else {
-                return password;
-            }
-        }
-        catch (error) {
-            console.log("error" + error)
-        }
-        try {
-            if (!password.match(condition3)) {
-                throw "password should have at least one lowercase letter"
-            }
-            else {
-                return password;
-            }
-        }
-        catch (error) {
-            console.log("error" + error)
-        }
-        try {
-            if (!password.match(condition4)) {
-                throw "password should have at least one uppercase letter"
-            }
-            else {
-                return password;
-            }
-        }
-        catch (error) {
-            console.log("error" + error)
-        }
-        try {
-            if (!password.match(condition5)) {
-                throw "password should have at least one symbol"
-            }
-            else {
-                return password;
-            }
-        }
-        catch (error) {
-            console.log("error" + error)
-        }
-        
-        try {
-            if (!password.match(condition6)) {
-                throw "password should have at least one digit"
-            }
-            else {
-                return password;
-            }
-        }
-        catch (error) {
-            console.log("error" + error)
-        }
-        
+  } catch (error) {
+    console.log("error" + error);
+  }
+  try {
+    if (password.length <= 8) {
+      throw "password should be longer than than 8 characters";
+    } else {
+      return password;
     }
-       console.log(passwordIsValid())
+  } catch (error) {
+    console.log("error" + error);
+  }
+  try {
+    if (!password.match(condition1) == null) {
+      throw "password should have at least one lowercase letter";
+    } else {
+      return password;
+    }
+  } catch (error) {
+    console.log("error" + error);
+  }
+  try {
+    if (!password.match(condition2) == null) {
+      throw "password should have at least one uppercase letter";
+    } else {
+      return password;
+    }
+  } catch (error) {
+    console.log("error" + error);
+  }
+  try {
+    if (!password.match(condition3) == null) {
+      e;
+      throw "password should have at least one symbol";
+    } else {
+      return password;
+    }
+  } catch (error) {
+    console.log("error" + error);
+  }
+
+  try {
+    if (!password.match(condition4) == null) {
+      throw "password should have at least one digit";
+    } else {
+      return password;
+    }
+  } catch (error) {
+    console.log("error" + error);
+  }
+}
+console.log(passwordIsValid("N4472157o#"));
 
 function passwordIsOk(password) {
-    // password = "N4472157o#";
-    if (
-        password.toString() !=="" &&
-        password.toString().length >= 9 &&
-        (!password.toString().match(condition3) ||
-            !password.toString().match(condition4) ||
-            !password.toString().match(condition5) ||
-            !password.toString().match(condition6))
-    ) {
-        return true;
-    } 
-    
-    else {
-        return false;
-    }
+  //  password = "N4472157o#";
+  if (
+    password !== "" &&
+    password.length > 8 &&
+    (!password.match(condition1) ||
+      !password.match(condition2) ||
+      !password.match(condition3) ||
+      !password.match(condition4))
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
-// console.log(passwordIsOk())
+console.log(passwordIsOk("N4472157o#"));
 
-module.exports = {passwordIsValid, passwordIsOk}
+module.exports = { passwordIsValid, passwordIsOk };
